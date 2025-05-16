b548ff7a6134ad481cc153ac402153ae505bf959
guotai <guotaixiao87@gmail.com>
2025-05-16 20:59:43 +1000

    change url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     const fetchProducts = async () => {
       setIsLoading(true);
-      const response = await fetch('https://backend-nu-six-26.vercel.app/products');
+      const response = await fetch('https://backend-nu-six-26.vercel.ap/products');
 
       const responseData = await response.json();
 
